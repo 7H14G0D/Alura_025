cde0e7a78217edf0238df51bad14329876258c97
Thiago Menezes <thiagobrunomenezesdacosta@gmail.com>
2023-02-24 11:27:00 -0300

    025

---
+++ alura-docs-main/src/dbConnect.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+const cliente = new MongoClient(
+  "mongodb+srv://alura:123@aluracluster.lp6gdyc.mongodb.net/?retryWrites=true&w=majority"
+);
+
+let documentosColecao;
+
+try {
+  await cliente.connect();
+
+  const db = cliente.db("alura-websockets");
+  documentosColecao = db.collection("documentos");
+
+  console.log("Conectado ao banco de dados com sucesso!");
+} catch (erro) {
+  console.log(erro);
+}
+
+export { documentosColecao };
